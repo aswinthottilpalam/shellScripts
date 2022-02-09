83dd9a3ce82626171e7bf5751fd41be4d11cf4d0
Aswin P <85921826+aswinthottilpalam@users.noreply.github.com>
2022-02-09 16:50:45 +0530

    Update dayMonthBtwnMarchJune.sh

--- dayMonthBtwnMarchJune.sh
+++ dayMonthBtwnMarchJune.sh
@@ -8,13 +8,11 @@ read -p " Enter Month: " Month
 if (( ($Month <= 6 & $date <= 20) ))
 then
 	echo "True";
-
+	
 elif (( ($Month >= 3 & $Month < 6) & ($date<31)  ))
 then
 	echo "True";
-
 else
-
 	echo "False";
 fi
 
